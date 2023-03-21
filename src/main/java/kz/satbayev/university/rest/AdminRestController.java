92c54afc3ae71185e75b340544c53a815cbc9e31
b.tursunkulov <@Zhakypali.Ayaulym>
2023-03-21 15:29:34 +0600

    update

--- src/main/java/kz/satbayev/university/rest/AdminRestController.java
+++ src/main/java/kz/satbayev/university/rest/AdminRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 @RestController
@@ -41,6 +42,7 @@ public class AdminRestController {
         List<User> users = userService.getAll();
         List<AdminUserDto> results = new ArrayList<>();
         for (User user : users) {
+            if(Objects.equals(user.getRoles().get(0).getName(), "ROLE_ADMIN"))continue;
             AdminUserDto result = AdminUserDto.fromUser(user);
             results.add(result);
         }
